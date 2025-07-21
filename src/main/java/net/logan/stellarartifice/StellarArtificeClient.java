bf303494e258ae04ed363ade726590e48a2d8d17
Unitato2k <54334663+Unitato2k@users.noreply.github.com>
2025-07-20 22:15:51 -0700

    Initial commit

---
+++ src/main/java/net/logan/stellarartifice/StellarArtificeClient.java
@@ -0,0 +1,10 @@
+package net.logan.stellarartifice;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StellarArtificeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/logan/stellarartifice/StellarArtificeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.logan.stellarartifice;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StellarArtificeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
